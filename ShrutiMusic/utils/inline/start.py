cb589031b04f8ae9c321dfaa9c9bcafb12cb0e50
uCoderr <nandyaduwanshi00@gmail.com>
2025-06-13 19:33:03 +0530

    Add files via upload

---
+++ ShrutiMusic/utils/inline/start.py
@@ -0,0 +1,42 @@
+from pyrogram.types import InlineKeyboardButton
+
+import config
+from ShrutiMusic import app
+
+
+def start_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+            ),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
+        ],
+    ]
+    return buttons
+
+
+def private_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["S_B_3"],
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(text=_["N_Y_1"], callback_data="LG"),
+            InlineKeyboardButton(
+    text=_["N_Y_2"], 
+    callback_data="fork_repo"
+            )
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")
+        ],
+    ]
+    return buttons
